09ca6c6914b0e35a4a80fd652c3b88c112120fb5
Inam54 <inamurrehman634@gmail.com>
2026-02-05 23:23:17 +0500

    Initial commit: BMW Sales Classification ML project

--- BMW_Sales_Prediction.py
+++ BMW_Sales_Prediction.py
@@ -149,7 +149,7 @@ class BMWSalesData:
             ))
         ])
 
-        #Splitting the data into features and target variable 
+        # Splitting the data into features and target variable 
         X = self.data.drop(['Sales_Classification', 'Sales_Volume'], axis=1)
         y = self.data['Sales_Classification']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
