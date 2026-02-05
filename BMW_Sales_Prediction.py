a5c0a80cf1616266101e12c99bbc91d8d61fc7af
Inam54 <inamurrehman634@gmail.com>
2026-02-05 23:34:12 +0500

    Initial commit: BMW Sales Classification ML project

--- BMW_Sales_Prediction.py
+++ BMW_Sales_Prediction.py
@@ -59,7 +59,7 @@ class BMWSalesData:
             ('Model' , LogisticRegression(max_iter=1000, class_weight='balanced'))
         ])
 
-        #Splitting the data into features and target variable 
+        # Splitting the data into features and target variable 
         X = self.data.drop(['Sales_Classification', 'Sales_Volume'], axis=1)
         y = self.data['Sales_Classification']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -104,7 +104,7 @@ class BMWSalesData:
 )
         ])
 
-        #Splitting the data into features and target variable 
+        # Splitting the data into features and target variable 
         X = self.data.drop(['Sales_Classification', 'Sales_Volume'], axis=1)
         y = self.data['Sales_Classification']
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
